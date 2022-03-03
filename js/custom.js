e499511fec19718ea2c1d7b18628f24cb2076049
Christ0x-ICyS <86194167+Christ0x-ICyS@users.noreply.github.com>
2022-03-02 19:06:08 -0500

    Formato js

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,16 @@
 function getCurrentYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
-    console.log(currentYear);
+  var currentDate = new Date();
+  var currentYear = currentDate.getFullYear();
+  document.querySelector("#displayYear").innerHTML = currentYear;
+  console.log(currentYear);
 }
 
 getCurrentYear();
 
 // lightbox
-$(document).on('click', '[data-toggle="lightbox"]', function(event) {
-    event.preventDefault();
-    $(this).ekkoLightbox({
-        alwaysShowClose: true,
-    });
-    
+$(document).on("click", '[data-toggle="lightbox"]', function (event) {
+  event.preventDefault();
+  $(this).ekkoLightbox({
+    alwaysShowClose: true,
+  });
 });
